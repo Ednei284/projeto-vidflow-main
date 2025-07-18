7ac0f8ea98d3930bb845d5e88da0a3ff28cc65c0
Ednei284 <metal2024heavy@gmail.com>
2025-07-18 15:45:13 -0300

    aula_3

--- script.js
+++ script.js
@@ -2,22 +2,46 @@ const containerVideos = document.querySelector(".videos__container");
 
 
 async function buscarEMostrarVideos() {
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
+    try {
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+        videos.forEach((video) => {
+            if (video.categoria == "") {
+                throw new Error("Video sem categoria.");
 
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-                <li class="videos__item">
-                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                        <div class="descricao-video">
-                                <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
-                                <h3 class="titulo-video">${video.titulo}</h3>
-                                <p class="titulo-canal">${video.descricao}</p>
-                        </div>
-                </li>
-                `;
-    })
+            }
+            containerVideos.innerHTML += `
+                    <li class="videos__item">
+                            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                            <div class="descricao-video">
+                                    <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                                    <h3 class="titulo-video">${video.titulo}</h3>
+                                    <p class="titulo-canal">${video.descricao}</p>
+                            </div>
+                    </li>
+                    `;
+        })
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
 
 buscarEMostrarVideos();
+
+const pesquisar__input = document.querySelector('.pesquisar__input')
+pesquisar__input.addEventListener('input', pesquisar)
+function pesquisar() {
+    const videos = document.querySelectorAll(".videos__item")
+    if (pesquisar__input.value != '') {
+        for (let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = pesquisar__input.value.toLowerCase();
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
+    }
+}
